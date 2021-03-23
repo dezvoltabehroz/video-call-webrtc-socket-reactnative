da5299b84a88e7d73e7e351228d43c5c231ee480
Behroz Ahmed <56107022+dezvoltabehroz@users.noreply.github.com>
2021-03-24 01:26:47 +0500

    Revert "Update App.js"
    This reverts commit 1b2fc14e8dddbb514e8a84d6a60a48487db7338b.

--- App.js
+++ App.js
@@ -99,15 +99,15 @@ export default class App extends Component {
 
       if (rtcPeerConnection.remoteDescription.type == "answer") {
 
-        // remoteStream = new MediaStream();
-        // rtcPeerConnection._remoteStreams[0].getTracks().forEach((track) => {
-        //   remoteStream.addTrack(track);
-        // });
+        remoteStream = new MediaStream();
+        rtcPeerConnection._remoteStreams[0].getTracks().forEach((track) => {
+          remoteStream.addTrack(track);
+        });
 
-        // finalStream = new MediaStream();
-        // rtcPeerConnection._localStreams[0].getTracks().forEach((track) => {
-        //   finalStream.addTrack(track);
-        // });
+        finalStream = new MediaStream();
+        rtcPeerConnection._localStreams[0].getTracks().forEach((track) => {
+          finalStream.addTrack(track);
+        });
 
         // console.log(finalStream)
         // console.log(remoteStream)
@@ -262,7 +262,7 @@ export default class App extends Component {
             <>
               <View style={{ height: 200, width: 200 }}>
                 <RTCView
-                  streamURL={localStream?.toURL()}
+                  streamURL={finalLocalStream?.toURL()}
                   style={styles.localVideo}
                 />
               </View>
