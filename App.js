c513b03c877aa98f0b0b39a91b8976a47b87124e
Behroz Ahmed <56107022+dezvoltabehroz@users.noreply.github.com>
2021-03-23 22:32:33 +0500

    Update App.js

--- App.js
+++ App.js
@@ -111,6 +111,7 @@ export default class App extends Component {
     })
 
     socket.on('webrtc_ice_candidate', (event) => {
+      console.log("event : ", event)
       if (event.candidate) {
         const candidate = new RTCIceCandidate(event.candidate)
         this.setState({ remoteCandidate: candidate })
